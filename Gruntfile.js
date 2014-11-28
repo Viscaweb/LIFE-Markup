e4f0a8d7efcf85892c554100921943ec65273081
Damien Gavard <damien@viscaweb.com>
2014-11-28 10:43:47 +0100

    Improve grunt config to allow more features

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,45 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         less: {
-            options: {
-                sourceMap: true,
-                outputSourceFiles: true
+            development: {
+                options: {
+                    //paths: ["css"]
+                },
+                files: {
+                    "css/style.css": "css/style.less"
+                }
             },
-            lessFiles: {
-                expand: true,
-                flatten: false,
-                src: ['css/style.less'],
-                dest: '',
-                ext: '.css'
+            production: {
+                options: {
+                    //paths: ["css"],
+                    cleancss: true
+                    //modifyVars: {
+                    //    imgPath: '"http://mycdn.com/path/to/images"',
+                    //    bgColor: 'red'
+                    //}
+                },
+                files: {
+                    "css/style.css": "css/style.less"
+                }
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'css/style.min.css': ['css/style.css']
+                }
             }
         },
         watch: {
             files: "**/*.less",
-            tasks: ["less"]
+            tasks: ["less:development"]
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
-
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['less:development']);
+    grunt.registerTask('build', ['less:production', 'cssmin']);
 };
