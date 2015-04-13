5b7dc46cd272897716151fa74ab413c2a9dc92de
Sébastien Aumonier <seb.aumonier@gmail.com>
2015-04-13 16:53:42 +0200

    LIMA-90 : new line to generate html layout from twig templates into temporary test files

--- src/index.php
+++ src/index.php
@@ -6,4 +6,8 @@ $loader = new Twig_Loader_Filesystem('views');
 
 $twig = new Twig_Environment($loader);
 
+
+file_put_contents('../compiled/index-twig.html', $twig->render('index.html.twig'));
+file_put_contents('../compiled/league-summary-twig.html', $twig->render('league-summary.html.twig'));
+
 ?>
