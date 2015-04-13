509fca8d639d4913199c918f443b803b6c6b3b40
Sébastien Aumonier <seb.aumonier@gmail.com>
2015-04-13 19:18:27 +0200

    LIMA-90 : generate html page from twig templates into temporary test files for added layouts

--- src/index.php
+++ src/index.php
@@ -9,5 +9,9 @@ $twig = new Twig_Environment($loader);
 
 file_put_contents('../compiled/index-twig.html', $twig->render('index.html.twig'));
 file_put_contents('../compiled/league-summary-twig.html', $twig->render('league-summary.html.twig'));
+file_put_contents('../compiled/match-head-to-head-twig.html', $twig->render('match-head-to-head.html.twig'));
+file_put_contents('../compiled/match-summary-twig.html', $twig->render('match-summary.html.twig'));
+file_put_contents('../compiled/match-summary-match-tips-iframe-twig.html', $twig->render('match-summary-match-tips-iframe.html.twig'));
+file_put_contents('../compiled/match-summary-live-commentary-iframe-twig.html', $twig->render('match-summary-live-commentary-iframe.html.twig'));
 
 ?>
