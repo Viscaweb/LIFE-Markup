87c1e45bae7eaf8d4a06bb97c48ad32a53318056
Sébastien Aumonier <seb.aumonier@gmail.com>
2015-04-30 17:18:12 +0200

    LIMA-123 : new Matches Results stats block with repartition charts

--- compiled/js/google-charts.js
+++ compiled/js/google-charts.js
@@ -4,7 +4,7 @@ google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'],
+        ['Type of OU', 'Number of matches'],
         ['Over 2,5 Goals',     16],
         ['Under 2,5 Goals',      8]
     ]);
@@ -22,4 +22,46 @@ function drawChart() {
 
     var chart = new google.visualization.PieChart(document.getElementById('chart_OU'));
     chart.draw(data, options);
+
+
+    var data2 = google.visualization.arrayToDataTable([
+        ['Match results', 'Number of matches'],
+        ['Win',     22],
+        ['Draw',      8],
+        ['Lost',      12]
+    ]);
+
+    var options2 = {
+        pieHole: 0.5,
+        pieSliceText: 'value',
+        fontSize: 12,
+        height:150,
+        legend: 'none',
+        chartArea:{left:0,top:0, width:'100%',height:'100%'},
+        colors:['#87b762', '#cacaca' , '#d9534f']
+    };
+
+    var chart2 = new google.visualization.PieChart(document.getElementById('chart_team_results_1'));
+    chart2.draw(data2, options2);
+
+
+    var data3 = google.visualization.arrayToDataTable([
+        ['Match results', 'Number of matches'],
+        ['Win',     16],
+        ['Draw',      8],
+        ['Lost',      8]
+    ]);
+
+    var options3 = {
+        pieHole: 0.5,
+        pieSliceText: 'value',
+        fontSize: 12,
+        height:150,
+        legend: 'none',
+        chartArea:{left:0,top:0, width:'100%',height:'100%'},
+        colors:['#87b762', '#cacaca' , '#d9534f']
+    };
+
+    var chart3 = new google.visualization.PieChart(document.getElementById('chart_team_results_2'));
+    chart3.draw(data3, options3);
 }
