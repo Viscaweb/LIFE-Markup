14d8e19cd9c86263785aac27bb76ff7e07bb21d6
Sébastien Aumonier <seb.aumonier@gmail.com>
2015-04-30 18:38:52 +0200

    LIMA-122 : Update Team OU Stats widget with chart + organizing blocks in team-summary.html

--- compiled/js/google-charts.js
+++ compiled/js/google-charts.js
@@ -1,28 +1,8 @@
 // Google charts declarations
 
-google.load("visualization", "1", {packages:["corechart"]});
+google.load("visualization", "1", {packages:["corechart", "bar"]});
 google.setOnLoadCallback(drawChart);
 function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Type of OU', 'Number of matches'],
-        ['Over 2,5 Goals',     16],
-        ['Under 2,5 Goals',      8]
-    ]);
-
-    var options = {
-        pieHole: 0.5,
-        pieSliceText: 'value',
-        fontSize: 12,
-        legend: {
-            position: 'bottom',
-        },
-        chartArea:{left:0,top:20, width:'100%',height:'80%'},
-        colors:['#5da400', '#4A8200']
-    };
-
-    var chart = new google.visualization.PieChart(document.getElementById('chart_OU'));
-    chart.draw(data, options);
-
 
     var data2 = google.visualization.arrayToDataTable([
         ['Match results', 'Number of matches'],
@@ -64,4 +44,38 @@ function drawChart() {
 
     var chart3 = new google.visualization.PieChart(document.getElementById('chart_team_results_2'));
     chart3.draw(data3, options3);
+
+
+
+
+    var data4 = new google.visualization.DataTable();
+    data4.addColumn('timeofday', 'Time of Day');
+    data4.addColumn('number', 'Motivation Level');
+    data4.addColumn('number', 'Energy Level');
+
+    data4.addRows([
+        [{v: [8, 0, 0], f: '8 am'}, 1, .25],
+        [{v: [9, 0, 0], f: '9 am'}, 2, .5],
+    ]);
+
+    var data4 = google.visualization.arrayToDataTable([
+        ['Year', 'Over 2,5 Goals', 'Under 2,5 Goals'],
+        ['2013/2014',     20, 18],
+        ['2013/2014',     18, 16]
+    ]);
+
+    var options = {
+        legend: {position:'bottom'},
+        fontSize: 12,
+        chartArea:{left:0,top:0, width:'100%',},
+        colors:['#5da400', '#4A8200']
+
+    };
+
+    var chart = new google.visualization.ColumnChart(
+        document.getElementById('chart_OU'));
+
+    chart.draw(data4, options);
+
+
 }
