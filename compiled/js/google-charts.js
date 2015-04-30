fa3c5682713e1e6f40ee9d04bbee2cc1e13598fa
Sébastien Aumonier <seb.aumonier@gmail.com>
2015-04-30 15:13:33 +0200

    LIMA-122 : adding widget Team O/U Stats with chart

--- compiled/js/google-charts.js
+++ compiled/js/google-charts.js
@@ -1,1 +1,25 @@
 // Google charts declarations
+
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Over 2,5 Goals',     16],
+        ['Under 2,5 Goals',      8]
+    ]);
+
+    var options = {
+        pieHole: 0.5,
+        pieSliceText: 'value',
+        fontSize: 12,
+        legend: {
+            position: 'bottom',
+        },
+        chartArea:{left:0,top:20, width:'100%',height:'80%'},
+        colors:['#5da400', '#4A8200']
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('chart_OU'));
+    chart.draw(data, options);
+}
