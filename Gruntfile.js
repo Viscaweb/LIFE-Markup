61941fb4fcbb1c5ce9001ec5b7f61de431090f29
MiguelMich <miguel_mich666@hotmail.com>
2015-08-02 19:30:07 -0500

    Added autoprefix task & load-grunt-tasks in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,7 @@
 module.exports = function (grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
     grunt.initConfig({
         less: {
             development: {
@@ -44,17 +47,31 @@ module.exports = function (grunt) {
 		      }
 		    },
 		},
+        autoprefixer: {
+          options: {
+            browsers: [
+              "Android >= 4",
+              "Chrome >= 20",
+              "Firefox >= 24",
+              "Explorer >= 8",
+              "iOS >= 6",
+              "Opera >= 12",
+              "Safari >= 6"
+            ]
+          },
+          dev: {
+            options: {
+              map: true
+            },
+            src: "compiled/css/style.css"
+          }
+        },
         watch: {
             files: "**/*.less",
-            tasks: ["less:development","csssplit:split", "cssmin"]
+            tasks: ["less:development", "autoprefixer", "csssplit:split", "cssmin"]
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-csssplit');
-
-    grunt.registerTask('default', ['less:development']);
+    grunt.registerTask('default', ["less:development", "autoprefixer", "csssplit:split", "cssmin"]);
     grunt.registerTask('build', ['less:production', 'cssmin']);
 };
